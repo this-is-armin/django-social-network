a19596bac5b8e388b147cb010689ae957ff32fc2
armin <this.is.armin.emami@gmail.com>
2025-09-23 22:59:42 +0330

    second commit

--- project/settings.py
+++ project/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
-from django.conf.global_settings import LOGIN_URL, LOGIN_REDIRECT_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
